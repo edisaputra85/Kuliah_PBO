48299b6c18610cf36d556f05459f978b3e04c274
edisaputra85 <47785870+edisaputra85@users.noreply.github.com>
2020-10-12 17:37:35 +0700

    Add files via upload

---
+++ BangunDatarInterface/src/bangundatarinterface/Lingkaran.java
@@ -0,0 +1,24 @@
+package bangundatarinterface;
+public class Lingkaran implements BangunDatar {
+   public final double PHI = 3.14;
+    //deklarasi atribut jejari memiliki lingkup akses publik
+    private double jejari;
+    //getter getJejari() disediakan untuk kelas lain yang ingin mendapatkan nilai atribut jejari 
+    public double getJejari(){
+       return jejari;   
+    }
+    //setter setJejari() disediakan untuk kelas lain yang ingin memodifikasi nilai atribut jejari 
+    public void setJejari(double jejari){
+       this.jejari = jejari;   
+    }
+    //implementasi method abstrak hitungKeliling()
+    @Override
+    public double hitungKeliling(){
+        return 2*PHI*jejari;//rumus keliling lingkaran
+    }
+    //implementasi method abstrak hitungLuas()
+    @Override
+    public double hitungLuas(){
+        return PHI*jejari*jejari;//rumus luas persegi
+    }  
+}
